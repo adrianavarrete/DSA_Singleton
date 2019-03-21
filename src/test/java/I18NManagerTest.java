36b08140022f9e8f660c0a05619140f0ad9b28c6
Adria <adri.navarrete.09@gmail.com>
2019-03-21 16:58:51 +0100

    acabado

--- src/test/java/I18NManagerTest.java
+++ src/test/java/I18NManagerTest.java
@@ -1,7 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class I18NManagerTest {
 
     @Test
     public void test() {
-        String r = I18NManager.getInstance().getText("en","l1")
+        String r = I18NManager.getInstance().getEtiqueta("en","l1");
+        Assert.assertEquals("Hello",r);
+
     }
 }
